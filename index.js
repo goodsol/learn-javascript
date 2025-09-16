bae243a5e4a4c3f6028387d8838b664cd7386646
goodsol <jes7959@gmail.com>
2025-09-16 09:55:16 +0900

    first commit

---
+++ index.js
@@ -0,0 +1,15 @@
+// 상수(변할 수 없는 값)
+const username = "조은솔"; 
+console.log("🚀 ~ username:", username); // turbo console insert legacy
+
+// 변수(변할 수 없는 값)
+let usermbti = "ESFJ";
+console.log("🚀 ~ usermbti:", usermbti);
+
+usermbti = "ISTP";
+console.log("🚀 ~ 변경된 usermbti:", usermbti);
+
+/* 상수(const)는 값 변경 불가능
+username = "존솔"; 
+console.log("🚀 ~ username:", username);
+*/
